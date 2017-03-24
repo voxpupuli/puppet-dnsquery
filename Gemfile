0cf33ebb68b2f1b7ba224d0cd8762286a4daf771
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-03-24 23:33:08 +0100

    Fix dns_ptr spec

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source ENV['GEM_SOURCE'] || 'https://rubygems.org'
 
 group :development, :test do
-  gem 'rake'
-  gem 'puppetlabs_spec_helper'
+  gem 'facter'
   gem 'puppet', '~> 4'
   gem 'puppet-blacksmith'
+  gem 'puppetlabs_spec_helper'
+  gem 'rake'
   gem 'rspec-puppet'
 end
--- spec/functions/dns_ptr_spec.rb
+++ spec/functions/dns_ptr_spec.rb
@@ -2,24 +2,10 @@
 
 require 'spec_helper'
 
-describe 'the dns_ptr function' do
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
-
-  it 'should exist' do
-    expect(Puppet::Parser::Functions.function('dns_ptr')).to eq('function_dns_ptr')
-  end
-
-  it 'should raise a ArgumentError if there is less than 1 arguments' do
-    expect { scope.function_dns_ptr([]) }.to raise_error(ArgumentError)
-  end
-
+describe 'dns_ptr' do
   it 'should return a list of PTR results when doing a lookup' do
-    results = scope.function_dns_ptr(['8.8.8.8.in-addr.arpa'])
+    results = subject.execute('8.8.8.8.in-addr.arpa')
     expect(results).to be_a Array
-    expect(results).to all( be_a String)
-  end
-
-  it 'should raise an error on empty reply' do
-    expect { scope.function_dns_ptr(['foo.example.com']) }.to raise_error(Resolv::ResolvError)
+    expect(results).to all(be_a(String))
   end
 end
